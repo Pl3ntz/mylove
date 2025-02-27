77a6be451684364fa4f22082b34ab369a373f420
Vitor Plentz <82619026+Pl3ntz@users.noreply.github.com>
2025-02-26 21:46:26 -0300

    update playlist

--- src/SpotifyPlayer.tsx
+++ src/SpotifyPlayer.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 export function SpotifyPlayer() {
   return (
-    <iframe
-      style={{ borderRadius: '12px' }}
-      src="https://open.spotify.com/embed/playlist/SUA_PLAYLIST_ID?utm_source=generator"
-      width="300"
-      height="380"
-      frameBorder="0"
-      allowFullScreen={false}
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    <iframe style="border-radius:12px" 
+    src="https://open.spotify.com/embed/playlist/7i1fVTRhzG4kLhAxEzNbf7?utm_source=generator&theme=0"
+    width="100%"
+    height="152"
+    frameBorder="0"
+    allowFullScreen={false}
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+    loading="lazy"
     />
   );
 }
+
+
+
